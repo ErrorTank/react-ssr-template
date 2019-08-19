17d2f7287e422ae85dcf375d901e6c63738370e0
Tuan Anh <ncq998@gmail.com>
2019-08-19 12:01:19 +0700

    end initial config

--- next.config.js
+++ next.config.js
@@ -15,6 +15,14 @@ module.exports = withCSS(withStylus({
 		return config
 	},
 	dir: './style',
+	webpackDevMiddleware: (config) => {
+		// Solve compiling problem via vagrant
+		config.watchOptions = {
+			poll: 1000,   // Check for changes every second
+			aggregateTimeout: 300,   // delay before rebuilding
+		};
+		return config;
+	}
 
 }));
 
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 
 const Home = (props) => {
+    console.log(process.env.PORT)
     return (
-        <div>dasdadadas</div>
+        <div>dsa21</div>
     );
 };
 
